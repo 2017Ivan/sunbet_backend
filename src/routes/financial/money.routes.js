406ee60282a133121bfd6c32321a46d86a1e29fc
mike101944 <fixedmatchese037@gmail.com>
2026-08-22 19:14:23 +0300

    feat:changes made for admin

--- src/routes/financial/money.routes.js
+++ src/routes/financial/money.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { authenticate } = require('../../middleware/auth.middleware');
+const { authenticate,authorize } = require('../../middleware/auth.middleware');
 const moneyController = require('../../controllers/financial/money.controller');
 
 // ============ PUBLIC WEBHOOKS ============
