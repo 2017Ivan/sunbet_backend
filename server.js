f7933813a159ed5ffff5be7dd8b068c887743936
mike101944 <fixedmatchese037@gmail.com>
2026-09-07 12:25:57 +0300

    feat:changes made

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: '100mb', extended: true }));
 
 /* =========================
    SOCKET.IO CONFIGURATION
